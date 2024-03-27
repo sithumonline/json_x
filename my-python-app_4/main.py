c21c869981eac61814003258c967050c2cfa9322
Sithum Bopitiya <sithumsandeepap@gmail.com>
2024-03-27 13:30:03 +0530

    Get coordinates using pygraphviz

--- my-python-app_4/main.py
+++ my-python-app_4/main.py
@@ -1,5 +1,8 @@
 import uuid
+
 import dearpygui.dearpygui as dpg
+import matplotlib.pyplot as plt
+import networkx as nx
 
 
 class JsonNode:
@@ -53,6 +56,33 @@ def process_list(lst, parent_node, key, level):
             parent_node.add_list(key, item)
 
 
+def json_node_to_nodes_and_links(node, G):
+    if node.key:
+        G.add_node(node.id, label=node.key)
+    elif node.values:
+        G.add_node(node.id, label="\n".join(f"{k}: {v}" for k, v in node.values.items()))
+    else:
+        G.add_node(node.id)
+
+    if node.parent_id:
+        G.add_edge(node.parent_id, node.id)
+
+    for key, value in node.lists.items():
+        G.add_node(node.id + "_" + key, label=key)
+        G.add_edge(node.id, node.id + "_" + key)
+
+        for i, item in enumerate(value):
+            G.add_node(node.id + f"_{key}_{i}", label=str(item))
+            G.add_edge(node.id + "_" + key, node.id + f"_{key}_{i}")
+
+    if node.values and node.key:
+        G.add_node(node.id + "_values", label="\n".join(f"{k}: {v}" for k, v in node.values.items()))
+        G.add_edge(node.id, node.id + "_values")
+
+    for child in node.children:
+        json_node_to_nodes_and_links(child, G)
+
+
 # callback runs when user attempts to connect attributes
 def link_callback(sender, app_data):
     # app_data -> (link_id1, link_id2)
@@ -67,20 +97,20 @@ def delink_callback(sender, app_data):
 
 def create_nodes2(node, parent=None):
     if node.key:
-        with dpg.node(parent=node_editor):
+        with dpg.node(parent=node_editor, pos=(pos[node.id][0], pos[node.id][1])):
             with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Output) as key_node:
                 dpg.add_text(node.key)
             with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Input) as key_node_:
                 pass
     elif node.values:
-        with dpg.node(parent=node_editor):
+        with dpg.node(parent=node_editor, pos=(pos[node.id][0], pos[node.id][1])):
             with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Output) as key_node:
                 txt = "\n".join(f"{k}: {v}" for k, v in node.values.items())
                 dpg.add_text(txt)
             with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Input) as key_node_:
                 pass
     else:
-        with dpg.node(parent=node_editor):
+        with dpg.node(parent=node_editor, pos=(pos[node.id][0], pos[node.id][1])):
             with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Output) as key_node:
                 pass
             with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Input) as key_node_:
@@ -90,7 +120,7 @@ def create_nodes2(node, parent=None):
         dpg.add_node_link(parent, key_node_, parent=node_editor)
 
     for key, value in node.lists.items():
-        with dpg.node(parent=node_editor):
+        with dpg.node(parent=node_editor, pos=(pos[node.id + "_" + key][0], pos[node.id + "_" + key][1])):
             with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Input) as key_input:
                 dpg.add_node_link(key_node, key_input, parent=node_editor)
 
@@ -98,13 +128,13 @@ def create_nodes2(node, parent=None):
                 dpg.add_text(key)
 
         for i, item in enumerate(value):
-            with dpg.node(parent=node_editor):
+            with dpg.node(parent=node_editor, pos=(pos[node.id + f"_{key}_{i}"][0], pos[node.id + f"_{key}_{i}"][1])):
                 with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Input) as key_input_:
                     dpg.add_text(str(item))
                     dpg.add_node_link(key_output, key_input_, parent=node_editor)
 
     if node.values and node.key:
-        with dpg.node(parent=node_editor):
+        with dpg.node(parent=node_editor, pos=(pos[node.id + "_values"][0], pos[node.id + "_values"][1])):
             with dpg.node_attribute(attribute_type=dpg.mvNode_Attr_Input) as key_value_node:
                 txt = "\n".join(f"{k}: {v}" for k, v in node.values.items())
                 dpg.add_text(txt)
@@ -170,9 +200,16 @@ json_data = {
 root_node = parse_json(json_data)
 print(root_node)
 
+G = nx.DiGraph()
+json_node_to_nodes_and_links(root_node, G)
+pos = nx.nx_agraph.graphviz_layout(G, prog="dot")
+# print(pos)
+nx.draw(G, pos, with_labels=True)
+plt.show()
+
 dpg.create_context()
 
-window = dpg.add_window(label="Json_x", width=400, height=400)
+window = dpg.add_window(label="Json_x", width=800, height=600)
 node_editor = dpg.add_node_editor(parent=window, callback=link_callback, delink_callback=delink_callback)
 create_nodes2(root_node)
 
